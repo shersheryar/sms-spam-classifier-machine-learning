2f1c4790882655e73fe6386b83003ddc6639a78c
Sheryar Sher <21p827@pwr.nu.edu.pk>
2025-05-13 04:57:08 +0500

    created pickle files for our model and made a streamlit for our model

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pickle 
+import nltk
+from nltk.corpus import stopwords
+from string import punctuation
+from nltk.stem import PorterStemmer
+ps = PorterStemmer()
+nltk.download('punkt')
+nltk.download('stopwords')
+# fucntions
+def transform_text(text):
+    text = text.lower()  # converting to lowercase
+    text = nltk.word_tokenize(text)  # tokenizing the text
+    text = [word for word in text if word.isalnum()]  # removing special characters
+    text = [word for word in text if word not in stopwords.words("english") and word not in punctuation] 
+    ps = PorterStemmer()  # creating an object of the PorterStemmer class
+    text = [ps.stem(word) for word in text]  # stemming the words
+    return " ".join(text)
+
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('spam_model.pkl', 'rb'))
+
+st.title("Email/SMS Spam Classifier")
+input_sms = st.text_area("Enter the message here", key="input")
+
+if st.button("Predict"):
+    # preprocess the input
+    transform_sms = transform_text(input_sms)
+    # vectorize the input
+    vector_input = tfidf.transform([transform_sms])
+    # predict
+    result = model.predict(vector_input)[0]
+    # display the result
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam") 
+
+
+ 
